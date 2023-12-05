e234d75bcd1b66571921ea80a47cd24a5344ff3b
MyoungHwaShin <shinmyounghwa92@gmail.com>
2023-12-05 16:37:31 +0900

    Feat: client에 CRUD용 스키마 이름 추가하였습니다.

--- src/client.ts
+++ src/client.ts
@@ -1,11 +1,18 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
+export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
 );
 
 
-export default supabase
+export const TABLE_NAME = {
+  video: 'video_comment',
+  user: 'user',
+  
+};
+
+
+
 
 
