df40c5179ad9053a59cfe8247be0b72614015847
uniS2 <s2oie1010@gmail.com>
2023-12-06 13:48:03 +0900

    Feat: 헤더 토글 메뉴바 아이템 이벤트 컴포넌트 생성

---
+++ src/components/MainPage/MenuItemEvent.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+function MenuItemEvent() {
+  return <StyledDiv />;
+}
+
+export default MenuItemEvent;
+
+const StyledDiv = styled.div`
+  width: 0.625rem;
+  height: 0.625rem;
+  border-radius: 50%;
+  background-color: #c90000;
+`;
