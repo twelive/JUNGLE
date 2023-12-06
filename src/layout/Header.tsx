147924841e4d8b3cd60b92317ed3b467db33a6c7
uniS2 <s2oie1010@gmail.com>
2023-12-06 16:53:25 +0900

    Feat: 헤더 컴포넌트 구현 완료 - `Logo`, `HeaderButtonGroup`, `HeaderTitle`, `HeaderMenu` 컴포넌트로 작성 - `isMenu`, `isBorder` props 통한 layout 또는 border CSS 조절

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,24 +1,38 @@
-import { NavLink } from 'react-router-dom';
+import HeaderMenu from '@components/Header/HeaderMenu';
+import HeaderTitle from '@components/Header/HeaderTitle';
+import Logo from '@components/Logo';
+import HeaderButtonGroup from '@components/Header/HeaderButtonGroup';
 import styled from 'styled-components';
 
-function Header() {
+function Header({ isMenu = true, isBorder = true }) {
   return (
-    <header>
-      <StyledNav>
-        <NavLink to="/">MainPage</NavLink>
-        <NavLink to="/job">JobPage</NavLink>
-        <NavLink to="/study">StudyPage</NavLink>
-        <NavLink to="/mypage">MyPage</NavLink>
-        <NavLink to="/community">CommunityPage</NavLink>
-        <NavLink to="/introduction">IntroductionPage</NavLink>
-      </StyledNav>
-    </header>
+    <HeaderSecton $isBorder={isBorder}>
+      <FlexBox>
+        <Logo isPoint={false} size="small" />
+        <HeaderButtonGroup />
+      </FlexBox>
+      <HeaderTitle>내 활동.</HeaderTitle>
+      {isMenu && <HeaderMenu />}
+    </HeaderSecton>
   );
 }
 
 export default Header;
 
-const StyledNav = styled.nav`
+const HeaderSecton = styled.header<{ $isBorder: boolean }>`
   display: flex;
-  gap: 1rem;
+  width: 100wh;
+  padding: 3.125rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 3.125rem;
+  border-bottom: ${(props) =>
+    props.$isBorder ? '0.15rem solid var(--bs-black-400)' : 'none'};
+`;
+
+const FlexBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
 `;
