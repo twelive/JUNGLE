7278fa2bc30e4317e01024b99a8c26668f6b2bb1
MyoungHwaShin <shinmyounghwa92@gmail.com>
2023-12-06 15:55:53 +0900

    Feat: useDataStore에 CRUD와 getUser추가 하였습니다.

--- src/store/useDataStore.ts
+++ src/store/useDataStore.ts
@@ -1,23 +1,33 @@
 import create from 'zustand';
 import { supabase } from '@/client';
+import { User } from '@supabase/supabase-js';
+
 
 type DataType = {
-  id: number;
+  id: number | string;
   [key: string]: number | string;
+  title: string;
+  created_at: string;
+  text: string;
+  
 };
 
 type State = {
   data: DataType[];
+  user: User | null;
   setData: (data: DataType[]) => void;
   getListData: (tableName: string) => Promise<void>;
   getIdData: (tableName: string, id: number) => Promise<void>;
   createData: (tableName: string, newData: DataType) => Promise<void>;
   updateData: (tableName: string, id: number, updatedData: DataType) => Promise<void>;
   deleteData: (tableName: string, id: number) => Promise<void>;
+  getUserData: () => Promise<void>;
 };
 
 const useDataStore = create<State>((set) => ({
+  
   data: [],
+  user: null,
   setData: (data: DataType[]) => set({ data }),
   getListData: async (tableName: string) => {
     const { data, error } = await supabase
@@ -44,7 +54,30 @@ const useDataStore = create<State>((set) => ({
         set({ data });
       }
     }
+
   },
+
+  getUserData: async () => {
+    const { data: { user } } = await supabase.auth.getUser();// 현재 로그인한 사용자의 정보를 가져옵니다.
+  
+    if (user) {
+      set({ user });  // 유저 정보를 상태에 저장합니다.
+    } else {
+      set({ user: null });  // 로그인하지 않은 경우에는 null을 상태에 저장합니다.
+    }
+  },
+  
+  // getUserData: async () => {
+  //   const { data: { user } } = await supabase.auth.getUser()
+  //   if (user !== null) {
+  //     set({ user });
+  //   }else {
+  //     set({ user });
+  //   }
+    
+  // },
+
+  
   createData: async (tableName: string, newData: DataType) => {
     const { data, error } = await supabase
       .from(tableName)
