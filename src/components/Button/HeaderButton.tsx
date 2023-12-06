f8561e592606ba025ca59737006606e13ebf5952
uniS2 <s2oie1010@gmail.com>
2023-12-06 16:22:45 +0900

    Feat: 헤더 버튼 컴포넌트 작성 - `borderWeight`, `backgroundColor`, `color` props 통한 CSS 변경 - `children` prop

---
+++ src/components/Button/HeaderButton.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+function HeaderButton({
+  borderWeight = '0.0625rem',
+  backgroundColor = 'black',
+  color = 'white',
+  children = 'HeaderButton',
+}) {
+  return (
+    <StyledButton
+      $borderWeight={borderWeight}
+      $backgroundColor={backgroundColor}
+      $color={color}
+    >
+      {children}
+    </StyledButton>
+  );
+}
+
+export default HeaderButton;
+
+const StyledButton = styled.button<{
+  $borderWeight: string;
+  $backgroundColor: string;
+  $color: string;
+}>`
+  display: inline-flex;
+  padding: 0.4375rem 1.1875rem;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.9375rem;
+  border: ${(props) => props.$borderWeight} solid var(--bs-black-100);
+  background-color: ${(props) => props.$backgroundColor};
+  color: ${(props) => props.$color};
+  font-size: 1.5rem;
+  text-align: center;
+  cursor: pointer;
+`;
