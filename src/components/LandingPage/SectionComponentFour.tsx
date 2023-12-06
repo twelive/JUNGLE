6d500ca324e419522624ad136d029dfcc92dac1b
Jeon SunYong <sunyong0725@naver.com>
2023-12-06 13:54:29 +0900

    MarkUp, Style : SectionComponentFour 1차 완성 SectionComponentFour 마크업 및 스타일링 완성

--- src/components/LandingPage/SectionComponentFour.tsx
+++ src/components/LandingPage/SectionComponentFour.tsx
@@ -1,5 +1,8 @@
 import { motion } from 'framer-motion';
 import { Element, Link } from 'react-scroll';
+import styled from 'styled-components';
+import test from '@/assets/landing/landing-test.svg';
+import DownButton from '@/assets/landing/landing-scroll-down.svg';
 
 const SectionComponentFour = () => {
   return (
@@ -15,11 +18,52 @@ const SectionComponentFour = () => {
           color: 'white',
         }}
       >
-        .
+        <MainBox>
+          <TextBox>
+            <Text>다양한 이력서 템플릿을 제공해드려요!</Text>
+            <Text>원하시는걸 고른 후 이력서를 작성해보세요!</Text>
+          </TextBox>
+          <ImgDiv>
+            <Img src={test} alt="" />
+          </ImgDiv>
+        </MainBox>
+        <ScrollButton>
+          <Link to="section5" smooth={true} duration={500}>
+            <img src={DownButton} alt="스크롤 버튼" />
+          </Link>
+        </ScrollButton>
       </motion.section>
-      <Link to="section5" smooth={true} duration={500}></Link>
     </Element>
   );
 };
 
 export default SectionComponentFour;
+
+const MainBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TextBox = styled.div`
+  padding-top: 50px;
+`;
+
+const Text = styled.p`
+  font-size: 40px;
+  text-align: center;
+  line-height: 80px;
+`;
+
+const ImgDiv = styled.div``;
+
+const Img = styled.img``;
+
+const ScrollButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  bottom: 30px;
+  right: 50px;
+`;
