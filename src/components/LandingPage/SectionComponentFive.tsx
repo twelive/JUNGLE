d42bc266e62bad5987b8ab662f3dd097d5a1a7aa
Jeon SunYong <sunyong0725@naver.com>
2023-12-07 15:32:37 +0900

    SectionComponentFive zustand 추가 , 애니메이션 변경 SectionComponentFour -> SectionComponentFive animation 실행을 위한 zustand 추가 SectionComponentFive 애니메이션 변경

--- src/components/LandingPage/SectionComponentFive.tsx
+++ src/components/LandingPage/SectionComponentFive.tsx
@@ -3,43 +3,121 @@ import { Element, Link } from 'react-scroll';
 import TopButton from '@/assets/landing/landing-scroll-up.svg';
 import styled from 'styled-components';
 import Logo from '@/assets/landing/landing-logo.svg';
+import useLandingStore from '@/store/useLandingStore';
 
 const SectionComponentFive = () => {
+  const showAnimationOne = useLandingStore(
+    (state) => state.showAnimationSectionOne
+  );
+  const showAnimationTwo = useLandingStore(
+    (state) => state.showAnimationSectionTwo
+  );
+
+  const showAnimationThree = useLandingStore(
+    (state) => state.showAnimationSectionThree
+  );
+
+  const showAnimationFour = useLandingStore(
+    (state) => state.showAnimationSectionFour
+  );
+
+  const setShowAnimationSectionFive = useLandingStore(
+    (state) => state.setShowAnimationSectionFive
+  );
+
+  const handleButtonClick = () => {
+    if (
+      showAnimationOne &&
+      showAnimationTwo &&
+      showAnimationThree &&
+      showAnimationFour
+    ) {
+      setShowAnimationSectionFive(true);
+    }
+  };
+
+  const childVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1, ease: 'easeInOut' },
+    },
+  };
+
+  const childVariantsTwo = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 2, ease: 'easeInOut' },
+    },
+  };
+
+  const childVariantsThree = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 3, ease: 'easeInOut' },
+    },
+  };
   return (
     <Element name="section5" className="element">
-      <motion.section
-        initial={{ opacity: 0, y: '-100vh' }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: '100vh' }}
-        style={{
-          width: '100%',
-          height: '100vh',
-          backgroundColor: 'black',
-          color: 'white',
-        }}
-      >
+      <MainSection>
         <MainBox>
-          <LogoBox>
-            <LogoImg src={Logo} alt="" />
-          </LogoBox>
-          <TextBox>
-            <Text>현명하게 취업준비 하는 방법</Text>
-          </TextBox>
-          <ButtonBox>
-            <Button>Join to JUNGLE</Button>
-          </ButtonBox>
+          <motion.div
+            variants={childVariants}
+            initial="hidden"
+            animate={showAnimationFour ? 'visible' : 'hidden'}
+          >
+            <LogoBox>
+              <LogoImg src={Logo} alt="JUNGLE 로고" />
+            </LogoBox>
+          </motion.div>
+          <motion.div
+            variants={childVariantsTwo}
+            initial="hidden"
+            animate={showAnimationFour ? 'visible' : 'hidden'}
+          >
+            <TextBox>
+              <Text>현명하게 취업준비 하는 방법</Text>
+            </TextBox>
+          </motion.div>
+          <motion.div
+            variants={childVariantsThree}
+            initial="hidden"
+            animate={showAnimationFour ? 'visible' : 'hidden'}
+          >
+            <ButtonBox>
+              <Button>Join to JUNGLE</Button>
+            </ButtonBox>
+          </motion.div>
         </MainBox>
         <ScrollButton>
-          <Link to="section1" smooth={true} duration={500}>
+          <Link
+            to="section1"
+            smooth={true}
+            duration={500}
+            onClick={handleButtonClick}
+          >
             <img src={TopButton} alt="스크롤 버튼" />
           </Link>
         </ScrollButton>
-      </motion.section>
+      </MainSection>
     </Element>
   );
 };
 
 export default SectionComponentFive;
+
+const MainSection = styled.div`
+  background-color: black;
+  color: white;
+  width: 100%;
+  height: 100vh;
+  position: relative;
+`;
 
 const MainBox = styled.div`
   display: flex;
@@ -57,16 +135,18 @@ const LogoImg = styled.img`
 `;
 
 const TextBox = styled.div`
-  padding-top: 20px;
+  padding-top: 50px;
 `;
 
 const Text = styled.p`
   font-size: 30px;
   font-weight: 500;
+  text-align: center;
 `;
 
 const ButtonBox = styled.div`
   padding-top: 50px;
+  text-align: center;
 `;
 
 const Button = styled.button`
