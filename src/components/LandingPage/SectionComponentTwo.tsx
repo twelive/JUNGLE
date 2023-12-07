d7540df2d70dd5db03ab166c939bd9ec8adc4ebf
Jeon SunYong <sunyong0725@naver.com>
2023-12-07 15:28:20 +0900

    Feat :SectionComponentTwo zustand 추가 , 애니메이션 변경 SectionComponentTwo -> SectionComponentThree animation 실행을 위한 zustand 추가 SectionComponentTwo 애니메이션 변경

--- src/components/LandingPage/SectionComponentTwo.tsx
+++ src/components/LandingPage/SectionComponentTwo.tsx
@@ -4,64 +4,79 @@ import styled from 'styled-components';
 import { Element, Link } from 'react-scroll';
 import DownButton from '@/assets/landing/landing-scroll-down.svg';
 import People from '@/assets/landing/landing-people.svg';
+import useLandingStore from '@/store/useLandingStore';
 
 const SectionComponentTwo = () => {
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { staggerChildren: 0.4 } },
+  const showAnimationOne = useLandingStore(
+    (state) => state.showAnimationSectionOne
+  );
+  const setShowAnimationSectionTwo = useLandingStore(
+    (state) => state.setShowAnimationSectionTwo
+  );
+
+  const handleButtonClick = () => {
+    setShowAnimationSectionTwo(true);
   };
 
   const childVariants = {
     hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 2, ease: 'easeInOut' }, // 수정된 부분: duration을 1.5초로 설정
+    },
   };
 
   return (
     <Element name="section2" className="element">
-      <motion.section
-        variants={containerVariants}
-        initial="hidden"
-        animate="visible"
-        style={{
-          backgroundColor: 'black',
-          color: 'white',
-          width: '100%',
-          height: '100vh',
-          position: 'relative',
-        }}
-      >
+      <MainSection>
         <LandingHeader />
         <MainDiv>
-          <motion.div variants={childVariants}>
+          <motion.div
+            variants={childVariants}
+            initial="hidden"
+            animate={showAnimationOne ? 'visible' : 'hidden'}
+          >
             <FirstDiv>
               <FirstSpan>
                 시작과 마지막을 <br /> JUNGLE과 함께 하세요
               </FirstSpan>
             </FirstDiv>
-          </motion.div>
-          <motion.div variants={childVariants}>
             <SecondDiv>
               <motion.img
                 src={People}
                 alt="사람들"
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0, delay: 0.2 }}
+                transition={{ duration: 0, delay: 1 }}
               />
             </SecondDiv>
           </motion.div>
         </MainDiv>
         <ScrollButton>
-          <Link to="section3" smooth={true} duration={500}>
+          <Link
+            to="section3"
+            smooth={true}
+            duration={1000}
+            onClick={handleButtonClick}
+          >
             <img src={DownButton} alt="스크롤 버튼" />
           </Link>
         </ScrollButton>
-      </motion.section>
+      </MainSection>
     </Element>
   );
 };
 
 export default SectionComponentTwo;
+
+const MainSection = styled.div`
+  background-color: black;
+  color: white;
+  width: 100%;
+  height: 100vh;
+  position: relative;
+`;
 
 const MainDiv = styled.div`
   display: flex;
