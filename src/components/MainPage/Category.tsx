6b4f088424b38705e7a0678f147e1e12b576b71c
uniS2 <s2oie1010@gmail.com>
2023-12-07 16:08:58 +0900

    Feat: 레이아웃 위한 CategorySection 컴포넌트 생성 - title, context, href props를 통해 변경

---
+++ src/components/MainPage/Category.tsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+import ArrowScrollDown from '@components/ArrowScrollDown';
+import { Link } from 'react-router-dom';
+
+interface CategoryProps {
+  title?: string;
+  context?: string;
+  href: string;
+}
+
+function Category({
+  title = '카테고리 타이틀',
+  context = '카테고리 일부 내용 렌더링 + 클릭시 해당 글로 이동',
+  href,
+}: CategoryProps) {
+  return (
+    <CategorySection>
+      <CategoryTextBox>
+        <Title>{title}</Title>
+        <Context>{context}</Context>
+      </CategoryTextBox>
+      <LinkBox to={href}>
+        <ArrowScrollDown color="var(--bs-black-400)" />
+      </LinkBox>
+    </CategorySection>
+  );
+}
+
+export default Category;
+
+const CommonLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const CategorySection = styled(CommonLayout)`
+  position: relative;
+  width: 53.75rem;
+  height: 21.875rem;
+  justify-content: space-between;
+`;
+
+const CategoryTextBox = styled(CommonLayout)`
+  justify-content: center;
+  gap: 1.875rem;
+`;
+
+const LinkBox = styled(Link)`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+`;
+
+const Title = styled.h2`
+  font-size: 3rem;
+  font-weight: 600;
+`;
+
+const Context = styled.p`
+  font-size: 2rem;
+  font-weight: 500;
+`;
