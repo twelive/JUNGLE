660af7f04f8e8f4f411b342acad96de0a834f899
Jeon SunYong <sunyong0725@naver.com>
2023-12-07 15:24:52 +0900

    Refactor : EnterButton 스타일링 수정 EnterButton 스타일링 수정

--- src/components/Button/EnterButton.tsx
+++ src/components/Button/EnterButton.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 function EnterButton() {
   return (
     <ButtonDiv type="button">
-      <ButtonText>탐험하기</ButtonText>
+      <ButtonText>Join</ButtonText>
     </ButtonDiv>
   );
 }
@@ -14,11 +14,11 @@ const ButtonDiv = styled.button`
   width: 105px;
   height: 43px;
   border-radius: 15px;
-  background-color: #f7f7f7;
+  background-color: transparent;
   cursor: pointer;
 `;
 
 const ButtonText = styled.span`
   font-size: 24px;
-  color: #555555;
+  color: white;
 `;
