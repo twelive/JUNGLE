c7f324641eedf77cc4a5fe543156b7b39207e5fe
Jeon SunYong <sunyong0725@naver.com>
2023-12-07 15:29:22 +0900

    Feat : SectionComponentThree zustand 추가 , 애니메이션 변경 SectionComponentTwo -> SectionComponentThree animation 실행을 위한 zustand 추가 SectionComponentThree 애니메이션 변경

--- src/components/LandingPage/SectionComponentThree.tsx
+++ src/components/LandingPage/SectionComponentThree.tsx
@@ -6,60 +6,97 @@ import Job from '@/assets/landing/landing-job.svg';
 import Study from '@/assets/landing/landing-study.svg';
 import Community from '@/assets/landing/landing-community.svg';
 import LandingHeader from './LandingHeader';
+import useLandingStore from '@/store/useLandingStore';
 
 const SectionComponentThree = () => {
+  const showAnimationOne = useLandingStore(
+    (state) => state.showAnimationSectionOne
+  );
+  const showAnimationTwo = useLandingStore(
+    (state) => state.showAnimationSectionTwo
+  );
+  const setShowAnimationSectionThree = useLandingStore(
+    (state) => state.setShowAnimationSectionThree
+  );
+
+  const handleButtonClick = () => {
+    if (showAnimationOne && showAnimationTwo) {
+      setShowAnimationSectionThree(true);
+    }
+  };
+
+  const shakeVariants = {
+    hidden: {
+      y: 0,
+    },
+    visible: {
+      y: [-40, 20, -30, 15, -20, 10, -10, 5, 0], // 리듬을 느끼는 모션 배열
+      transition: {
+        duration: 20, // 애니메이션 속도를 느리게 조정 (20초)
+        ease: 'easeInOut',
+        loop: Infinity, // 무한 반복
+      },
+    },
+  };
+
   return (
     <Element name="section3" className="element">
-      <motion.section
-        initial={{ opacity: 0, y: '-100vh' }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: '100vh' }}
-        style={{
-          width: '100%',
-          height: '100vh',
-          backgroundColor: 'black',
-          color: 'white',
-        }}
-      >
+      <MainSection>
         <LandingHeader />
         <MainDiv>
           <TextBox>
             <TextStyle>많은 정보를 제공해 드릴게요. 함께 성장해요!</TextStyle>
           </TextBox>
-          <ImgBox>
-            <ImgBoxOne>
-              <ImgBoxTitle>Job</ImgBoxTitle>
-              <ImgBoxText>채용회사</ImgBoxText>
-              <ImgBoxText>면접</ImgBoxText>
-            </ImgBoxOne>
-            <ImgBoxTwo>
-              <ImgBoxTitle>Study</ImgBoxTitle>
-              <ImgBoxText>도서추천</ImgBoxText>
-              <ImgBoxText>기술스택</ImgBoxText>
-            </ImgBoxTwo>
-            <ImgBoxThree>
-              <ImgBoxTitle>Community</ImgBoxTitle>
-              <ImgBoxText>스터디</ImgBoxText>
-              <ImgBoxText>프로젝트</ImgBoxText>
-            </ImgBoxThree>
-          </ImgBox>
+          <motion.div
+            variants={shakeVariants}
+            initial="hidden"
+            animate={showAnimationTwo ? 'visible' : 'hidden'}
+          >
+            <ImgBox>
+              <ImgBoxOne>
+                <ImgBoxTitle>Job</ImgBoxTitle>
+                <ImgBoxText>채용회사</ImgBoxText>
+                <ImgBoxText>면접</ImgBoxText>
+              </ImgBoxOne>
+              <ImgBoxTwo>
+                <ImgBoxTitle>Study</ImgBoxTitle>
+                <ImgBoxText>도서추천</ImgBoxText>
+                <ImgBoxText>기술스택</ImgBoxText>
+              </ImgBoxTwo>
+              <ImgBoxThree>
+                <ImgBoxTitle>Community</ImgBoxTitle>
+                <ImgBoxText>스터디</ImgBoxText>
+                <ImgBoxText>프로젝트</ImgBoxText>
+              </ImgBoxThree>
+            </ImgBox>
+          </motion.div>
         </MainDiv>
         <ScrollButton>
-          <Link to="section4" smooth={true} duration={500}>
+          <Link
+            to="section4"
+            smooth={true}
+            duration={500}
+            onClick={handleButtonClick}
+          >
             <img src={DownButton} alt="스크롤 버튼" />
           </Link>
         </ScrollButton>
-      </motion.section>
+      </MainSection>
     </Element>
   );
 };
 
 export default SectionComponentThree;
 
+const MainSection = styled.div`
+  background-color: black;
+  color: white;
+  width: 100%;
+  height: 100vh;
+  position: relative;
+`;
+
 const MainDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 130px;
   padding-top: 120px;
 `;
 
@@ -70,6 +107,7 @@ const TextBox = styled.div`
 const TextStyle = styled.p`
   font-size: 40px;
   font-weight: 800;
+  padding-bottom: 120px;
 `;
 
 const ImgBox = styled.div`
