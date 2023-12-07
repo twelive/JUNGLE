42530ca36bb2b9651ed04b14a6a87dea7cec02e7
uniS2 <s2oie1010@gmail.com>
2023-12-07 13:15:54 +0900

    Feat: 로고 컴포넌트 href prop에 따른 Link, Div 요소 설정 - href prop을 가질 시 Link 컴포넌트로서 메인페이지로 이동합니다.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,17 +1,34 @@
+import { Link } from 'react-router-dom';
 import logoTitle from '@assets/common/logo-title.svg';
 import logoBorder from '@assets/common/logo-border.svg';
 import styled from 'styled-components';
 
-function Logo({ isPoint = true, size = 'default' }) {
+interface LogoProps {
+  isPoint?: boolean;
+  size?: string;
+  href?: string;
+}
+
+function Logo({ href, isPoint = true, size = 'default' }: LogoProps) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const ComponentName: React.ComponentType<any> = href ? StyledLink : FlexBox;
+
   return (
-    <FlexBox $size={size}>
+    <ComponentName to={href} $size={size}>
       <ImgBox src={logoTitle} $size={size} />
       {isPoint && <ImgBox src={logoBorder} $size={size} />}
-    </FlexBox>
+    </ComponentName>
   );
 }
 
 export default Logo;
+
+const StyledLink = styled(Link)<{ $size: string }>`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  gap: ${(props) => (props.$size === 'default' ? '2.75rem' : '1.375rem')};
+`;
 
 const FlexBox = styled.div<{ $size: string }>`
   display: inline-flex;
