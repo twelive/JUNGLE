9f5b3a78ef45bbe2b524d0218e8a4da0c42d8d38
uniS2 <s2oie1010@gmail.com>
2023-12-08 14:03:24 +0900

    Feat: 메인페이지 레이아웃 작업 완료

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,5 @@
 import { Helmet } from 'react-helmet-async';
-import Category from '@components/MainPage/Category';
+import CategorySection from '@components/MainPage/CategorySection';
 import styled from 'styled-components';
 
 function MainPage() {
@@ -8,14 +8,48 @@ function MainPage() {
       <Helmet>
         <title>Main - JUNGLE</title>
       </Helmet>
-      <Heading>JUNGLE</Heading>
-      <Category href="/mypage" />
+      <h1 className="sr-only">JUNGLE</h1>
+      <CategorySection />
+      <IntroductionSection>
+        <IntroductionBox>페이지 소개/슬로건</IntroductionBox>
+        <TeamBox>
+          <TeamTitle>JUNGLE</TeamTitle>
+          <TeamContent>팀 소개 또는 Github, Figma 링크 연결</TeamContent>
+        </TeamBox>
+      </IntroductionSection>
     </>
   );
 }
 
 export default MainPage;
 
-const Heading = styled.h1`
-  // sr-only
+const IntroductionSection = styled.div`
+  display: flex;
+  height: 28.125rem;
+  padding: 3.125rem;
+`;
+
+const IntroductionBox = styled.div`
+  width: 100%;
+  font-size: 4.5rem;
+  font-weight: 700;
+  align-self: center;
+`;
+
+const TeamBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.875rem;
+  border-left: 0.15rem solid var(--bs-black-400);
+  padding: 3.125rem;
+  width: 60%;
+`;
+
+const TeamTitle = styled.span`
+  font-size: 3rem;
+  font-weight: 600;
+`;
+
+const TeamContent = styled.p`
+  font-size: 2rem;
 `;
