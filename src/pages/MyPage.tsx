207e6e59c562d0592e916588e42280f79ae34e10
uniS2 <s2oie1010@gmail.com>
2023-12-11 17:42:23 +0900

    Feat: 메인페이지 마크업 완료

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,4 +1,11 @@
 import { Helmet } from 'react-helmet-async';
+import styled from 'styled-components';
+import ProfileSection from '@components/MyPage/ProfileSection';
+import PointSection from '@components/MyPage/PointSection';
+import BookMarkLink from '@components/MyPage/BookMarkLink';
+import BookMarkList from '@components/MyPage/BookMarkList';
+import WritingCategory from '@components/MyPage/WritingCategory';
+import ResumeLink from '@components/MyPage/ResumeLink';
 
 function MyPage() {
   return (
@@ -6,11 +13,48 @@ function MyPage() {
       <Helmet>
         <title>MyPage - JUNGLE</title>
       </Helmet>
-      <section>
-        <h1>MyPage</h1>
-      </section>
+      <Section>
+        <h1 className="sr-only">내 활동</h1>
+        <TopSection>
+          <ProfileSection />
+          <PointSection />
+        </TopSection>
+        <BookMarkSection>
+          <BookMarkLink />
+          <BookMarkList />
+        </BookMarkSection>
+        <FlexLayout>
+          <WritingCategory />
+          <ResumeLink />
+        </FlexLayout>
+      </Section>
     </>
   );
 }
 
 export default MyPage;
+
+const Section = styled.section`
+  position: relative;
+
+  h2 {
+    font-size: 3rem;
+    font-weight: 600;
+  }
+`;
+
+const FlexLayout = styled.div`
+  display: flex;
+`;
+
+const TopSection = styled(FlexLayout)`
+  align-items: center;
+  gap: 3.125rem;
+  padding: 3.125rem;
+`;
+
+const BookMarkSection = styled(FlexLayout)`
+  width: 100%;
+  border-top: 0.15rem solid var(--bs-black-400);
+  border-bottom: 0.15rem solid var(--bs-black-400);
+`;
