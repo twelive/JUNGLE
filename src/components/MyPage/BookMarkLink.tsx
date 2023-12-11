b99ff3dc7a5592eb5a688f130fac782d401fe38b
uniS2 <s2oie1010@gmail.com>
2023-12-11 15:48:06 +0900

    Feat: 북마크 링크 영역 연결 - 추후 Supabase API 내 북마크 정보 Read 작업 및 개수 렌더링할 예정입니다. - 북마크 개수는 자동으로 업데이트되게 합니다. Related to #37

---
+++ src/components/MyPage/BookMarkLink.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import ArrowScrollDown from '@components/ArrowScrollDown';
+
+function BookMarkLink() {
+  return (
+    <BookMarkLinkBox>
+      <InnerBox>
+        <h2>북마크</h2>
+        <BookMarkButton type="button">
+          <ArrowScrollDown color="var(--bs-black-400)" />
+        </BookMarkButton>
+      </InnerBox>
+      {/* Supabase 에서 북마크 정보 불러오기 */}
+      <CountText>[n]개</CountText>
+    </BookMarkLinkBox>
+  );
+}
+
+export default BookMarkLink;
+
+const BookMarkLinkBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 50%;
+  min-width: 28.75rem;
+  padding: 3.125rem;
+`;
+
+const InnerBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const BookMarkButton = styled.button`
+  /* reset CSS */
+  border: none;
+  background-color: transparent;
+`;
+
+const CountText = styled.span`
+  align-self: stretch;
+  text-align: right;
+  font-size: 2rem;
+  font-weight: 600;
+`;
