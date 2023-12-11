ecc1e450e95d3fe5a3fe637f462b8f0913f57e7d
uniS2 <s2oie1010@gmail.com>
2023-12-11 15:01:00 +0900

    Feat: 마이페이지 내 프로필, 포인트 영역 마크업 완료 - 프로필: ProfileSction - 포인트: PointSection

---
+++ src/components/MyPage/ProfileSection.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import Profile from '@components/MyPage/Profile';
+
+function ProfileSection({ user = 'userName' }) {
+  return (
+    <ProfileBox>
+      <h2 className="sr-only">프로필</h2>
+      <Profile />
+      <ProfileText>
+        어서오세요 {user}님, <br /> 지금까지의 활동내역을 보여드립니다.
+      </ProfileText>
+    </ProfileBox>
+  );
+}
+
+export default ProfileSection;
+
+const ProfileBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 3.125rem;
+  width: 70%;
+  padding-right: 3.125rem;
+  border-right: 0.15rem solid var(--bs-black-400);
+`;
+
+const ProfileText = styled.p`
+  font-size: 3rem;
+  font-weight: 600;
+  line-height: 6.25rem;
+`;
