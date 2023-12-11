0acc612eb310ded4a246863013a41af02a611659
uniS2 <s2oie1010@gmail.com>
2023-12-12 04:07:22 +0900

    Feat: 이력서 페이지 경로 설정, 페이지 내 헤더 조건 처리 - `routes.tsx` : 경로 설정 - `RootLayout.tsx` : 경로가 '/mypage/resume'를 포함할 경우에만 `isMenu=false` (메뉴바 사라짐)

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -2,13 +2,16 @@ import { Outlet, useLocation } from 'react-router-dom';
 import MainHeader from '@layout/MainHeader';
 import Header from '@layout/Header';
 import Footer from '@layout/Footer';
+import getPathName from '@utils/getPathName';
 
 export default function RootLayout() {
   const { pathname } = useLocation();
+  console.log(pathname);
+  
 
   return (
     <>
-      {pathname !== '/' && (pathname === '/main' ? <MainHeader /> : <Header />)}
+      {pathname !== '/' && (pathname === '/main' ? <MainHeader /> : <Header isMenu={getPathName(pathname, 14) === '/mypage/resume' ? false : true } />)}
       <Outlet />
       {pathname !== '/' && <Footer />}
     </>
