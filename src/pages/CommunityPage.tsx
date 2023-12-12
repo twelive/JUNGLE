cdfe91b1b567d96257a8abe05ae78517797c85e3
seojinman <dirnrhd21@naver.com>
2023-12-12 09:56:36 +0900

    Feat: 배너 이미지 스와이퍼 구현 Grid 스와이퍼를 이용하여 프로젝트/스터티 구현 supabase 데이더 불러와서 화면에 렌더링 및 css

--- src/pages/CommunityPage.tsx
+++ src/pages/CommunityPage.tsx
@@ -18,9 +18,28 @@ import banner2 from '../assets/community/community-banner2.svg';
 import banner3 from '../assets/community/community-banner3.svg';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom'; 
-
+import useDataStore from '@/store/useDataStore';
+import { useEffect, useState } from 'react';
+import useStorageStore from '@/store/useStorageStore';
+import { getPbImageURL } from '@/store/getPbImageURL';
 
 function ComunityPage() {
+  const { data: projectData, getListData: getProjectListData } = useDataStore();
+  const { data: studyData, getListData: getStudyListData } = useDataStore();
+  const [dataType, setDataType] = useState('project'); // Initial data type is 'project'
+  const{getAllList} = useStorageStore();
+
+  useEffect(() => {
+    if (dataType === 'project') {
+      getProjectListData('community_project');
+    } else if (dataType === 'study') {
+      getStudyListData('community_study');
+    }
+    getAllList('community_img','');
+    
+  }, [dataType, getProjectListData, getStudyListData, getAllList]);
+
+  
 
   return (
     <>
@@ -70,11 +89,9 @@ function ComunityPage() {
       </section>
 
       <section>
-        <button>프로젝트</button>
-        <button>스터디</button>
-        <Link to="communitycreate">
-          모집하기
-        </Link>
+        <button onClick={() => setDataType('project')}>프로젝트</button>
+        <button onClick={() => setDataType('study')}>스터디</button>
+        <Link to="communitycreate">모집하기</Link>
         <SecondSwiperContainer>
           <SecondSwiper
             slidesPerView={3}
@@ -88,42 +105,37 @@ function ComunityPage() {
             modules={[Grid, Pagination]}
           >
             {/* SwiperSlide */}
-            <CustomSwiperSlide>
-              <SecondSlide>Slide 1</SecondSlide>
-            </CustomSwiperSlide>
-            <CustomSwiperSlide>
-              <SecondSlide>Slide 2</SecondSlide>
-            </CustomSwiperSlide>
-            <CustomSwiperSlide>
-              <SecondSlide>Slide 3</SecondSlide>
-            </CustomSwiperSlide>
-            <CustomSwiperSlide>
-              <SecondSlide>Slide 4</SecondSlide>
-            </CustomSwiperSlide>
-            <CustomSwiperSlide>
-              <SecondSlide>Slide 5</SecondSlide>
-            </CustomSwiperSlide>
-            <CustomSwiperSlide>
-              <SecondSlide>Slide 6</SecondSlide>
-            </CustomSwiperSlide>
-            <CustomSwiperSlide>
-              <SecondSlide>Slide 7</SecondSlide>
-            </CustomSwiperSlide>
-            <CustomSwiperSlide>
-              <SecondSlide>Slide 8</SecondSlide>
-            </CustomSwiperSlide>
-            <CustomSwiperSlide>
-              <SecondSlide>Slide 9</SecondSlide>
-            </CustomSwiperSlide>
-            <CustomSwiperSlide>
-              <SecondSlide>Slide 10</SecondSlide>
-            </CustomSwiperSlide>
-            <CustomSwiperSlide>
-              <SecondSlide>Slide 11</SecondSlide>
-            </CustomSwiperSlide>
-            <CustomSwiperSlide>
-              <SecondSlide>Slide 12</SecondSlide>
-            </CustomSwiperSlide>
+            {(dataType === 'project' ? projectData : studyData).map((item) => (
+              <CustomSwiperSlide key={item.id}>
+                <StyledLink to={`/detailPage/${item.id}`}>
+                  <SecondSlide>
+                    <Maincontents>
+                      <H2>{item.title}</H2>
+                      <P>마감일 : {item.deadline}</P>
+                      <Contents>{item.contents}</Contents>
+                      {/* Loop through tags */}
+                      <Imgwrapper>
+                      {[item.tag1, item.tag2, item.tag3].map(
+                        (tag, index) =>
+                        // Render image if tag exists
+                        tag && (
+                          <div key={index}>
+                              <Img
+                                src={getPbImageURL(
+                                  'community_img',
+                                  `${tag}.svg`
+                                  )}
+                                  />
+                            </div>
+                          )
+                          )}
+                          </Imgwrapper>
+                    </Maincontents>
+                    <div>{item.email}</div>
+                  </SecondSlide>
+                </StyledLink>
+              </CustomSwiperSlide>
+            ))}
           </SecondSwiper>
         </SecondSwiperContainer>
       </section>
@@ -154,34 +166,37 @@ const SecondSwiperContainer = styled.div`
   width: 80%;
   height: 80%;
   margin: 0 auto;
-`;
+  `;
 
 
 const SecondSwiper = styled(ReactSwiper)`
-  width: 80%;
+  width: 100%;
   height: 100%;
   .swiper-wrapper {
     display: grid;
     grid-template-columns: repeat(6, 1fr); 
     grid-template-rows: repeat(2, 1fr); 
-    gap: 20px; 
+    gap: 40px; 
     justify-items: center;
-    align-items: center;
     margin: 0 auto;
   }
-`;
+  `;
 
 const SecondSlide = styled.div`
   text-align: center;
   font-size: 18px;
   background: #fff;
   width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  max-width: 100%;
+  height: 300px;
   flex-direction: column;
   margin: 0;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: normal;
+  border-radius: 20px;
   .swiper-slide {
     margin: 0 !important;
   }
@@ -191,4 +206,67 @@ const SecondSlide = styled.div`
 const CustomSwiperSlide = styled(SwiperSlideDefault)`
   width: 50px;
   height: 50px;
+  :hover{
+    border: 3px solid #000;
+  }
+  `;
+
+const Img = styled.img`
+  width: 50px;
+  height: auto;
+  padding-right: 10px;
+  `;
+
+const H2 = styled.h2`
+  padding-top: 20px;
+  padding-bottom: 20px;
+  white-space: nowrap; /* Prevent wrapping */
+  overflow: hidden; /* Hide overflowing content */
+  text-overflow: ellipsis; /* Show ellipsis for overflowed content */
 `;
+
+const P = styled.p`
+  color: gray;
+  text-align: left;
+  padding-left: 10px;
+`;
+
+const Contents = styled.p`
+  text-align: left;
+  padding-left: 10px;
+  display: -webkit-box;
+  -webkit-line-clamp: 2; /* Limit to two lines */
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding-top: 20px;
+  height: 35px; /* Set a fixed height */
+`;
+
+const Imgwrapper = styled.div`
+  display: flex;
+  padding-left: 10px;
+  padding-top: 20px;
+  padding-bottom: 10px;
+
+  `;
+
+
+const StyledLink = styled(Link)`
+    text-decoration: none; /* Remove underline */
+    color: black; /* Set default text color */
+    &:visited {
+      color: black; /* Set visited link color */
+    }
+    &:hover {
+      text-decoration: none; /* Remove underline on hover */
+      color: black; /* Change text color on hover */
+    }
+    `;
+
+    const Maincontents = styled.div`
+      height: 240px; /* Set a fixed height */
+      overflow: hidden;
+      text-overflow: ellipsis;
+      border-bottom: 1px solid gray;
+      `;
