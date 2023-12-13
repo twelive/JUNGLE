12a563b86546195da63a91336eb1eb193be7e3ae
Jeon SunYong <sunyong0725@naver.com>
2023-12-13 13:33:15 +0900

    Feat : JobCodingTestBox 1차 완성 마크업 및 스타일링 완성
    추후 supabase 연결 구성

--- src/components/JobPage/JobCodingTestBox.tsx
+++ src/components/JobPage/JobCodingTestBox.tsx
@@ -1,5 +1,78 @@
+import styled, { keyframes } from 'styled-components';
+const hoverAnimation = keyframes`
+  0% {
+    box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+  }
+  100% {
+    box-shadow: 0 0 10px 5px rgba(0, 0, 0, 0.3);
+  }
+`;
 function JobCodingTestBox() {
-  return <div>123</div>;
+  return (
+    // map 돌릴 예정 h1 부분 , 날짜 , 작성자이름. title , createAt(slice 예정, 2023-12-10 까지), name
+    // MainBox 에 클릭하면 해당 페이지로 이동 예정.
+    <MainBox>
+      <TitleBox>
+        <Title>카카오 코딩테스트 본 후기</Title>
+      </TitleBox>
+      <SubBox>
+        <DateBox>2023-12-13</DateBox>
+        <UserName>정소이</UserName>
+      </SubBox>
+    </MainBox>
+  );
 }
 
 export default JobCodingTestBox;
+
+const MainBox = styled.div`
+  width: 100%;
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 0 15px;
+  cursor: pointer;
+  box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+  transition: box-shadow 0.3s ease-in-out;
+
+  &:hover {
+    animation: ${hoverAnimation} 0.3s ease-in-out forwards;
+  }
+`;
+
+const TitleBox = styled.div`
+  border-bottom: 1px solid black;
+`;
+
+const Title = styled.p`
+  font-size: 35px;
+  font-weight: 700;
+  padding: 50px 0;
+
+  @media ${(props) => props.theme.device.tablet} {
+    font-size: 28px;
+  }
+`;
+
+const SubBox = styled.div`
+  display: flex;
+  padding: 20px 0;
+`;
+
+const DateBox = styled.div`
+  border-right: 1px solid black;
+  font-size: 25px;
+  padding: 0 15px;
+
+  @media ${(props) => props.theme.device.tablet} {
+    font-size: 20px;
+  }
+`;
+
+const UserName = styled.div`
+  font-size: 25px;
+  padding-left: 15px;
+
+  @media ${(props) => props.theme.device.tablet} {
+    font-size: 20px;
+  }
+`;
