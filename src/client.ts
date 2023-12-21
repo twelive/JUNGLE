e895a780b3497fa9342d19d853c448f5722b3fc9
uniS2 <s2oie1010@gmail.com>
2023-12-21 10:22:56 +0900

    Docs: 절대 경로 반영

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "./types/database";
+import { Database } from "@/types/database";
 
 export const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
