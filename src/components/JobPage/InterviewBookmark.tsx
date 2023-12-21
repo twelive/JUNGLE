3ab8b410f09b4fb97ab818182de638980cdaa240
Jeon SunYong <sunyong0725@naver.com>
2023-12-22 01:48:57 +0900

    Feat : BookMark 생성 CodingTestBookmark , InterviewBookmark 생성

---
+++ src/components/JobPage/InterviewBookmark.tsx
@@ -0,0 +1,95 @@
+import styled from 'styled-components';
+import bookmark from './../../assets/common/bookmarkbluefilled.svg';
+import { useState, useEffect } from 'react';
+import { supabase } from '@/client';
+
+interface BookMarkButtonProps {
+  itemId: string | number;
+  userId: string | number | undefined;
+  itemType: string | number;
+  notBookmarkImg: string; // notBookmark 이미지 URL
+}
+
+function InterviewBookmark({
+  itemId,
+  userId,
+  itemType,
+  notBookmarkImg,
+}: BookMarkButtonProps) {
+  const initialBookMarks = JSON.parse(
+    localStorage.getItem(`job_interview_bookmark-${itemId}`) || 'false'
+  );
+
+  const [toggle, setToggle] = useState(initialBookMarks);
+
+  const updateBookMarks = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation(); // Stop event from bubbling up
+    e.preventDefault();
+    if (toggle) {
+      const { error } = await supabase
+        .from('job_interview_bookmark')
+        .delete()
+        .match({
+          user_id: userId as string,
+          [`${itemType}_id`]: itemId,
+        });
+
+      if (error) {
+        console.error('Error deleting like:', error.message);
+      } else {
+        setToggle(!toggle);
+        localStorage.setItem(`bookmark-${itemId}`, JSON.stringify(!toggle));
+      }
+    } else {
+      const { error } = await supabase.from('job_interview_bookmark').upsert({
+        user_id: userId as string,
+        [`${itemType}_id`]: itemId,
+      });
+
+      if (error) {
+        console.error('Error updating likes:', error.message);
+      } else {
+        setToggle(!toggle);
+        localStorage.setItem(`bookmark-${itemId}`, JSON.stringify(!toggle));
+      }
+    }
+  };
+
+  useEffect(() => {
+    const fetchBookMarksData = async () => {
+      if (userId) {
+        const { data, error } = await supabase
+          .from('job_interview_bookmark')
+          .select('user_id')
+          .eq('user_id', userId)
+          .eq(`${itemType}_id`, itemId);
+
+        if (error) {
+          console.error('Error fetching like status:', error.message);
+        } else {
+          setToggle(data.length > 0);
+        }
+      }
+    };
+
+    fetchBookMarksData();
+  }, [userId, itemId, itemType]);
+  return (
+    <Button onClick={updateBookMarks}>
+      <img
+        src={toggle ? bookmark : notBookmarkImg}
+        alt={toggle ? '북마크' : '북마크 취소'}
+      ></img>
+    </Button>
+  );
+}
+
+export default InterviewBookmark;
+
+const Button = styled.button`
+  background: transparent;
+  border: none;
+  position: absolute;
+  top: 10px;
+  right: 7px;
+`;
