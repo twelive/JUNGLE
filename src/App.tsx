3930b4cb84e839524e1e15b6f6370722c2e5854e
MyoungHwaShin <shinmyounghwa92@gmail.com>
2023-12-22 12:30:17 +0900

    Feat: App에 register와 세션 적용하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -8,37 +8,59 @@ import router from '@/routes';
 import theme from '@/theme';
 import GlobalStyles from '@/GlobalStyles';
 import LandingPage from '@pages/LandingPage/LandingPage';
-import { useAuthStore } from '@store/useAuthStore';
-import Loading from '@components/Loading';
 
+import Loading from '@components/Loading';
+import { supabase } from './client';
+
+import { Session } from '@supabase/supabase-js';
 const queryClient = new QueryClient();
 
 function App() {
-  const isAuth = useAuthStore((state) => state.isAuth);
+
+const [session, setSession] = useState<Session | null>(null);
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+      console.log("변경");
+    })
+    
+  }, []);
+
+
+
 
   return (
+ 
     <>
+          {!session ? (
+            
+              <LandingPage />
+            
+          ) : (
+              <>
       <ThemeProvider theme={theme}>
         <QueryClientProvider client={queryClient}>
-          {!isAuth && (
-            <>
-              <LandingPage />
-            </>
-          )}
-          {isAuth && (
-            <>
               <HelmetProvider>
                 <Suspense fallback={<Loading />}>
                   <RouterProvider router={router} />
                   <GlobalStyles />
                 </Suspense>
               </HelmetProvider>
-            </>
-          )}
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </ThemeProvider>
-    </>
+            </>
+              
+          )}
+          
+            </>
+
+          
   );
 }
 
