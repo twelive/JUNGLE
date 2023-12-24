a3378347f179491411938e4bc7e4308ce1c0ce92
uniS2 <s2oie1010@gmail.com>
2023-12-24 17:33:42 +0900

    Init: 프로모션, 개발 환경 리디렉션 경로 최종 설정

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -44,14 +44,17 @@ const initialAuthState = {
 
 export const useAuthStore = create<State>((set) => {
   const getURL = () => {
-    let url =
-      import.meta.env.VITE_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-      import.meta.env.VITE_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-      'http://localhost:3000/'
-    // Make sure to include `https://` when not localhost.
+    let url;
+    if (import.meta.env.MODE === 'production') {
+      url = import.meta.env.VITE_PUBLIC_SITE_URL // Set this to your site URL in production env.
+    } else {
+      url = import.meta.env.VITE_PUBLIC_VERCEL_URL // Automatically set by Vercel.
+    }
+
     url = url.includes('http') ? url : `https://${url}`
     // Make sure to include a trailing `/`.
     url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+    
     return url
   }
   
