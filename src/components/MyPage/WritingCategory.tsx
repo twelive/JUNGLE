efd9d1ccdf8c2e7ba03be5ee2f86dad836e8168f
uniS2 <s2oie1010@gmail.com>
2023-12-25 23:34:58 +0900

    Feat: 마이페이지 내 하단 영역 재배치

--- src/components/MyPage/WritingCategory.tsx
+++ src/components/MyPage/WritingCategory.tsx
@@ -26,8 +26,8 @@ function WritingCategory() {
 export default WritingCategory;
 
 const Layout = styled.div<{ $isBorder?: boolean }>`
- display: grid;
- grid-template-columns: 1fr 1fr;
+ display: flex;
+ flex-wrap: nowrap;
  min-width: 40.625rem;
  border-bottom: ${(props) =>
     props.$isBorder ? `0.15rem solid var(--bs-black-400)` : 'none'};
@@ -38,19 +38,37 @@ const Layout = styled.div<{ $isBorder?: boolean }>`
 `;
 
 const CategoryBox = styled.div`
+  width: 17.5rem;
   margin: 1.5625rem 3.125rem 1.5625rem 0;
   padding: 1.875rem;
   border-radius: 0.9375rem;
   background: white;
 
   @media ${(props) => props.theme.device.tablet} {
+    width: 15rem;
+    min-width: 15rem;
+    height: 17.5rem;
     margin: 1.25rem 2.5rem 1.25rem 0;
     padding: 1.25rem;
   }
 
   @media ${(props) => props.theme.device.mobile} {
-    width: 75vw;
+    width: 12.5rem;
+    min-width: 12.5rem;
+    height: 15rem;
     margin: 0.9375rem 0;
     padding: 0.625rem;
+  }
+
+  h2 {
+    font-size: 2em;
+
+    @media ${(props) => props.theme.device.tablet} {
+      font-size: 1.75rem;
+    }
+
+    @media ${(props) => props.theme.device.mobile} {
+      font-size: 1.5rem;
+    }
   }
 `;
