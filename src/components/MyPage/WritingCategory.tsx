dd64e8105dd1613eb1618b4ddee98ccbc8dda463
uniS2 <s2oie1010@gmail.com>
2024-01-16 10:06:06 +0900

    Style: 작성글 UI 스타일링 개선 (커뮤니티 기준)

--- src/components/MyPage/WritingCategory.tsx
+++ src/components/MyPage/WritingCategory.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import useWrittingData from '@/api/useWrittingData';
-import Category from '@components/MainPage/Category';
+import ResumeCategory from '@components/MyPage/ResumeCategory';
 
 function WritingCategory() {
   const { data } = useWrittingData();
@@ -12,11 +12,15 @@ function WritingCategory() {
         {data &&
           data.map((category, index) => (
             <StyledWritingWrapper key={index}>
-              <Category
+              <ResumeCategory
                 href={`/study/stack/detail/${category.id}`}
                 title={category.title || undefined}
                 context={category.text || undefined}
-                height={'19.375rem'}
+                date={
+                  category.updated_at
+                    ? category.updated_at
+                    : category.created_at
+                }
               />
             </StyledWritingWrapper>
           ))}
@@ -40,38 +44,20 @@ const StyledWritingContainer = styled.div<{ $isBorder?: boolean }>`
 `;
 
 const StyledWritingWrapper = styled.div`
-  min-width: 17.5rem;
-  width: 17.5rem;
-  margin: 1.5625rem 3.125rem 1.5625rem 0;
-  padding: 1.875rem;
+  min-width: 15rem;
+  width: 15rem;
+  height: 17.5rem;
+  margin: 1.25rem 2.5rem 1.25rem 0;
+  padding: 1.25rem;
   border-radius: 0.9375rem;
   background: white;
-
-  @media ${(props) => props.theme.device.tablet} {
-    min-width: 15rem;
-    width: 15rem;
-    height: 17.5rem;
-    margin: 1.25rem 2.5rem 1.25rem 0;
-    padding: 1.25rem;
-  }
+  color: var(--bs-black-200);
 
   @media ${(props) => props.theme.device.mobile} {
     min-width: 12.5rem;
     width: 12.5rem;
     height: 15rem;
-    margin: 0.9375rem 0;
-    padding: 0.625rem;
-  }
-
-  h2 {
-    font-size: 2rem !important;
-
-    @media ${(props) => props.theme.device.tablet} {
-      font-size: 1.75rem !important;
-    }
-
-    @media ${(props) => props.theme.device.mobile} {
-      font-size: 1.5rem !important;
-    }
+    margin: 1rem 0.5rem;
+    padding: 0.75rem;
   }
 `;
