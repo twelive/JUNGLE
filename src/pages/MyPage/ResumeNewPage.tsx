f840061831f119ed4860043a81a25e434d504af1
uniS2 <s2oie1010@gmail.com>
2024-02-05 05:26:24 +0900

    Feat: 기본 UI 수정 & API Create 작업
    - 기본정보 form 까지 UI 수정 완료
    - API Create 작업 검토 & 성공 확인

--- src/pages/MyPage/ResumeNewPage.tsx
+++ src/pages/MyPage/ResumeNewPage.tsx
@@ -8,6 +8,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import { useNavigate } from 'react-router-dom';
 import debounce from '@/utils/debounce';
 import { ResumeNewDTO } from '@/types/ResumeNew';
+import ResumeSubheading from '@/components/MyPage/ResumeSubheading';
+import ResumeInfoIcon from '@/components/MyPage/ResumeInfoIcon';
 
 const createData: (
   tableName: string,
@@ -25,42 +27,98 @@ function ResumeNewPage() {
   const userId = useAuthStore((state) => state.user);
   const userEmail = useAuthStore((state) => state.userEmail);
   const navigate = useNavigate();
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  console.log(title, content);
+  const [, setTitle] = useState('');
+  const [, setName] = useState('');
+  const [, setJob] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [, setPhone] = useState('');
+  const [, setMail] = useState('');
+  const [, setGithub] = useState('');
+  const [, setBlog] = useState('');
+  const [, setLink] = useState('');
+  const [linkCount, setLinkCount] = useState(0);
+  const [, setContent] = useState('');
   const [stack, setStack] = useState<
     'Javascript' | 'TypeScirpt' | 'React' | 'Next.js'
   >('Javascript');
 
-  const handleStackClick = () => {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const jobRef = useRef<HTMLInputElement>(null);
+  const phoneRef = useRef<HTMLInputElement>(null);
+  const mailRef = useRef<HTMLInputElement>(null);
+  const githubRef = useRef<HTMLInputElement>(null);
+  const blogRef = useRef<HTMLInputElement>(null);
+  const linkRef = useRef<HTMLInputElement>(null);
+  const projectRef = useRef<HTMLTextAreaElement>(null);
+  const SecureEmail = userEmail.replace(/@.*/, '');
+
+  const debouncedSetTitle = debounce((value: string) => setTitle(value), 500);
+  const debouncedSetName = debounce((value: string) => setName(value), 500);
+  const debouncedSetJob = debounce((value: string) => setJob(value), 500);
+  const debouncedSetPhone = debounce((value: string) => setPhone(value), 500);
+  const debouncedSetMail = debounce((value: string) => setMail(value), 500);
+  const debouncedSetGithub = debounce((value: string) => setGithub(value), 500);
+  const debouncedSetBlog = debounce((value: string) => setBlog(value), 500);
+  const debouncedSetLink = debounce((value: string) => setLink(value), 500);
+  const debouncedSetContent = debounce(
+    (value: string) => setContent(value),
+    500
+  );
+
+  function phoneFormat(phoneNumber: string) {
+    const value = phoneNumber.replace(/[^0-9]/g, '');
+
+    const firstLength = value.length > 9 ? 3 : 4;
+    const formatted = [
+      value.slice(0, firstLength),
+      value.slice(firstLength, firstLength + 4),
+      value.slice(firstLength + 4),
+    ].join('-');
+
+    return value.length > 9 ? formatted : value;
+  }
+
+  const handlePhoneInput = (e: React.FormEvent<HTMLInputElement>) => {
+    const formatted = phoneFormat(e.currentTarget.value);
+    setPhoneNumber(formatted);
+    debouncedSetPhone(formatted);
+  };
+
+  const handlePlusLink = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setLinkCount((prevLinks) => prevLinks + 1);
+  };
+
+  const handlePlusStack = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
     setStack('React');
   };
 
-  const titleRef = useRef<HTMLInputElement>(null);
-  const portfolioRef = useRef<HTMLInputElement>(null);
-  const projectRef = useRef<HTMLTextAreaElement>(null);
-  const activitiesRef = useRef<HTMLTextAreaElement>(null);
-  const SecureEmail = userEmail.replace(/@.*/, '');
-  const debouncedSetTitle = debounce((value: string) => setTitle(value), 50);
-  const debouncedSetContent = debounce(
-    (value: string) => setContent(value),
-    50
-  );
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const title = titleRef.current?.value;
-    const portfolio = portfolioRef.current?.value;
+    const name = nameRef.current?.value;
+    const job = jobRef.current?.value;
+    const phone = phoneRef.current?.value;
+    const mail = mailRef.current?.value;
+    const github = githubRef.current?.value;
+    const blog = blogRef.current?.value;
+    const link = linkRef.current?.value;
     const project = projectRef.current?.value;
-    const activities = activitiesRef.current?.value;
 
-    if (title && portfolio && userId && userEmail) {
+    if (title && name && job && userId) {
       const data = {
         title,
-        portfolio,
+        name,
+        job,
+        phone,
+        mail,
+        github,
+        blog,
+        link,
         project,
-        activities,
         user_id: userId,
-        user_email: userEmail,
       };
 
       try {
@@ -75,145 +133,273 @@ function ResumeNewPage() {
     }
   };
 
+  const handleTemporary = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+  };
+
   const handleReset = () => {
     titleRef.current!.value = '';
-    portfolioRef.current!.value = '';
+    nameRef.current!.value = '';
+    jobRef.current!.value = '';
     projectRef.current!.value = '';
-    activitiesRef.current!.value = '';
   };
 
   return (
     <>
-      <Helmet>createNew ResumePage</Helmet>
-      <NewOuter>
-        <TitleArea>이력서 작성</TitleArea>
-        <FormArea onSubmit={handleSubmit}>
-          <Label>
-            <AnyTextBox>
-              <Author>제목</Author>
-              <Input
-                type="text"
-                ref={titleRef}
-                onChange={(e) => debouncedSetTitle(e.target.value)}
+      <Helmet>
+        <title>createNew ResumePage</title>
+      </Helmet>
+      <h1 className="sr-only">새 이력서 작성</h1>
+      <StyledFormArea onSubmit={handleSubmit}>
+        <h2 className="sr-only">제목</h2>
+        <StyledTitleInput
+          type="text"
+          placeholder="제목을 입력해주세요."
+          required
+          ref={titleRef}
+          onChange={(e) => debouncedSetTitle(e.target.value)}
+        />
+        <ResumeSubheading essential>기본 정보</ResumeSubheading>
+        <StyledSection className="information">
+          <StyledInfoInput
+            type="text"
+            placeholder="이름을 입력해주세요."
+            required
+            ref={nameRef}
+            onChange={(e) => debouncedSetName(e.target.value)}
+          />
+          <StyledInfoInput
+            type="text"
+            placeholder="직무를 입력해주세요."
+            required
+            ref={jobRef}
+            onChange={(e) => debouncedSetJob(e.target.value)}
+          />
+          <span>개인정보</span>
+          <StyledPersonalContainer>
+            <StyledLabel>
+              <ResumeInfoIcon imgIcon="전화번호" />
+              <StyledPersonalInput
+                type="tel"
+                placeholder="숫자만 입력해주세요."
+                maxLength={14}
+                value={phoneNumber}
+                onInput={handlePhoneInput}
+                ref={phoneRef}
+                onChange={(e) => debouncedSetPhone(e.target.value)}
               />
-            </AnyTextBox>
-          </Label>
-          <Label>
-            <AnyTextBox>
-              <Author>포트폴리오</Author>
-              <Input
-                type="text"
-                ref={titleRef}
-                onChange={(e) => debouncedSetTitle(e.target.value)}
+            </StyledLabel>
+            <StyledLabel>
+              <ResumeInfoIcon imgIcon="메일" />
+              <StyledPersonalInput
+                type="email"
+                placeholder="이메일 주소를 입력해주세요."
+                ref={mailRef}
+                onChange={(e) => debouncedSetMail(e.target.value)}
               />
-            </AnyTextBox>
-            <StyledAddButton type="button">+ 추가</StyledAddButton>
-          </Label>
-
-          <Label>
-            <StyledToggleButton onClick={handleStackClick}>
-              주요 기술 스택
-            </StyledToggleButton>
-            <StyledToggleButton onClick={handleStackClick}>
-              자격증/외국어
-            </StyledToggleButton>
-          </Label>
-
-          <Label>
-            <AnyTextBox>
-              <Author>작성자</Author>
-              <Email>{SecureEmail}</Email>
-            </AnyTextBox>
-          </Label>
-          <Label>
-            <div>주요 프로젝트</div>
-            <Textarea
-              ref={projectRef}
-              onChange={(e) => debouncedSetContent(e.target.value)}
-            />
-          </Label>
-          <Label>
-            <div>활동</div>
-            <Textarea
-              ref={activitiesRef}
-              onChange={(e) => debouncedSetContent(e.target.value)}
-            />
-          </Label>
-          <ButtonArea>
-            <Button type="submit">저장</Button>
-            <Button type="button" onClick={handleReset}>
-              취소
-            </Button>
-          </ButtonArea>
-        </FormArea>
-      </NewOuter>
+            </StyledLabel>
+            <StyledLabel>
+              <ResumeInfoIcon imgIcon="깃허브" />
+              <StyledPersonalInput
+                type="url"
+                placeholder="깃허브 주소를 입력해주세요."
+                ref={githubRef}
+                onChange={(e) => debouncedSetGithub(e.target.value)}
+              />
+            </StyledLabel>
+            <StyledLabel>
+              <ResumeInfoIcon imgIcon="블로그" />
+              <StyledPersonalInput
+                type="url"
+                placeholder="블로그 주소를 입력해주세요."
+                ref={blogRef}
+                onChange={(e) => debouncedSetBlog(e.target.value)}
+              />
+            </StyledLabel>
+            {Array(linkCount)
+              .fill(null)
+              .map((_, index) => (
+                <StyledLabel>
+                  <ResumeInfoIcon />
+                  <StyledPersonalInput
+                    key={index}
+                    type="url"
+                    placeholder="추가 링크 주소를 입력해주세요."
+                    ref={linkRef}
+                    onChange={(e) => debouncedSetLink(e.target.value)}
+                  />
+                </StyledLabel>
+              ))}
+            <StyledPlusButton onClick={handlePlusLink}>
+              <ResumeInfoIcon />
+              링크 추가
+            </StyledPlusButton>
+          </StyledPersonalContainer>
+        </StyledSection>
+        <ResumeSubheading>기술 스택</ResumeSubheading>
+        <StyledSection>
+          <StyledPlusButton onClick={handlePlusStack}>
+            <ResumeInfoIcon />
+            기술 스택 추가
+          </StyledPlusButton>
+        </StyledSection>
+        <ResumeSubheading>경험/활동/교육</ResumeSubheading>
+        <label>
+          <ResumeSubheading>프로젝트</ResumeSubheading>
+          <Textarea
+            ref={projectRef}
+            onChange={(e) => debouncedSetContent(e.target.value)}
+          />
+        </label>
+        <ResumeSubheading>자기소개</ResumeSubheading>
+        <ResumeSubheading>포트폴리오</ResumeSubheading>
+        <ButtonArea>
+          <Button type="button" onClick={handleTemporary}>
+            임시저장
+          </Button>
+          <Button type="submit">저장</Button>
+          <Button type="button" onClick={handleReset}>
+            취소
+          </Button>
+        </ButtonArea>
+      </StyledFormArea>
     </>
   );
 }
 
 export default ResumeNewPage;
 
-const NewOuter = styled.section`
-  padding: 3.125rem;
-  color: black;
-
-  @media ${(props) => props.theme.device.mobile} {
-    padding-top: 1.25rem;
-    padding-left: 0;
-    padding-bottom: 0;
-    padding-right: 0;
-  }
-
-  select {
-    border: 1px solid black;
-  }
-`;
-
-const FormArea = styled.form`
-  margin: 5%;
+const StyledFormArea = styled.form`
   display: flex;
   flex-direction: column;
-  border: 1px solid black;
-  border-radius: 2.1875rem;
-  padding: 1.25rem;
-  min-height: 25rem;
+  gap: 1.25rem;
+  margin: 5% 0;
+  padding: 5%;
+  border: 3px solid gray;
+  border-radius: 1.25rem;
+  background: #efeee9;
 `;
 
-const Author = styled.div`
-  text-align: center;
-  white-space: nowrap;
-  padding-left: 0.3125rem;
-  padding-right: 0.9375rem;
-`;
-
-const Email = styled.p``;
-
-const AnyTextBox = styled.div`
-  width: 100%;
+const StyledSection = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  padding: 1.25rem;
+  border: 3px solid #444;
+  border-radius: 0.625rem;
 
-  justify-items: center;
-  vertical-align: middle;
+  @media ${(props) => props.theme.device.tablet} {
+    padding: 1.25rem;
+  }
+  @media ${(props) => props.theme.device.mobile} {
+    padding: 0.625rem;
+  }
+
+  span {
+    font-size: 2rem;
+    font-weight: 600;
+
+    @media ${(props) => props.theme.device.tablet} {
+      font-size: 1.75rem;
+    }
+    @media ${(props) => props.theme.device.mobile} {
+      font-size: 1.125rem;
+    }
+  }
+`;
+
+const StyledPersonalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.625rem;
+  width: auto;
+  margin-left: 0.625rem;
+`;
+
+const StyledLabel = styled.label`
+  display: flex;
+  gap: 0.625rem;
+  max-width: 50rem;
+  cursor: pointer;
+
+  @media ${(props) => props.theme.device.tablet} {
+    width: auto;
+  }
+  @media ${(props) => props.theme.device.mobile} {
+    width: auto;
+  }
+`;
+
+const StyledInput = styled.input`
+  padding: 0.3125rem 0.9375rem;
+  border: 1px solid #999;
+  border-radius: 0.3125rem;
+  background: white;
+  outline: none;
+
+  &:focus {
+    animation: round 1s forwards;
+  }
+
+  @keyframes round {
+    from {
+      border-radius: 0.3125rem;
+    }
+    to {
+      border-radius: 1.875rem;
+    }
+  }
+`;
+
+const StyledTitleInput = styled(StyledInput)`
+  font-size: 2.75rem;
+  font-weight: bold;
+
+  @media ${(props) => props.theme.device.tablet} {
+    font-size: 2.5rem;
+  }
+  @media ${(props) => props.theme.device.mobile} {
+    font-size: 1.5rem;
+  }
+`;
+
+const StyledInfoInput = styled(StyledInput)`
+  max-width: 50rem;
+  font-size: 2rem;
+  font-weight: 500;
+
+  @media ${(props) => props.theme.device.tablet} {
+    font-size: 1.75rem;
+  }
+  @media ${(props) => props.theme.device.mobile} {
+    font-size: 1.125rem;
+  }
+`;
+
+const StyledPersonalInput = styled(StyledInput)`
+  width: 100%;
+  font-size: 1.5rem;
+
+  @media ${(props) => props.theme.device.tablet} {
+    font-size: 1.25rem;
+  }
+  @media ${(props) => props.theme.device.mobile} {
+    font-size: 0.875rem;
+  }
+`;
+
+const StyledPlusButton = styled.button`
+  display: flex;
   align-items: center;
-  @media ${(props) => props.theme.device.mobile} {
-    font-size: 0.625rem;
-  }
-`;
-
-const Label = styled.label`
-  width: 100%;
-  display: block;
-  margin-bottom: 0.625rem;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  margin-top: 0.3125rem;
-  padding: 0.3125rem;
-  border: 1px solid black;
-  @media ${(props) => props.theme.device.mobile} {
-    padding: 0rem;
-  }
+  gap: 0.3125rem;
+  width: fit-content;
+  padding: 0.3125rem 0.9375rem;
+  border: none;
+  border-radius: 0.625rem;
+  box-shadow: 3px 3px 2px 1px rgba(137, 137, 138, 0.2);
+  background: white;
+  font-weight: 700;
+  cursor: pointer;
 `;
 
 const Textarea = styled.textarea`
@@ -227,14 +413,6 @@ const Textarea = styled.textarea`
   padding: 0.625rem;
 `;
 
-const TitleArea = styled.p`
-  font-weight: 600;
-  font-size: 3.125rem;
-  margin-left: 5%;
-  @media ${(props) => props.theme.device.mobile} {
-    font-size: 1.25rem;
-  }
-`;
 const ButtonArea = styled.div`
   display: flex;
   flex-direction: row;
@@ -256,26 +434,4 @@ const Button = styled.button`
   @media ${(props) => props.theme.device.mobile} {
     font-size: 0.625rem;
   }
-`;
-
-const StyledToggleButton = styled.button`
-  background-color: white;
-  border: none;
-  padding: 0.3125rem 0.9375rem;
-  border-radius: 0.625rem;
-  margin: 0.3125rem;
-  box-sizing: border-box;
-  font-weight: 700;
-  box-shadow: 3px 3px 2px 1px rgba(137, 137, 138, 0.2);
-`;
-
-const StyledAddButton = styled.button`
-  background-color: transparent;
-  border: none;
-  padding: 0.3125rem 0.9375rem;
-  border-radius: 0.625rem;
-  margin: 0.3125rem;
-  box-sizing: border-box;
-  font-weight: 700;
-  cursor: pointer;
 `;
