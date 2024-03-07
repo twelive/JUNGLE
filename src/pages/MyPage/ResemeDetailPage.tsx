50ef80ccbcc403a2f83c00b5a0bc380966c9e9ea
uniS2 <s2oie1010@gmail.com>
2024-03-07 22:22:17 +0900

    Feat: 이력서 세부 페이지 - 작성 완료

--- src/pages/MyPage/ResemeDetailPage.tsx
+++ src/pages/MyPage/ResemeDetailPage.tsx
@@ -1,329 +1,135 @@
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
 import styled from 'styled-components';
 import { supabase } from '@/client';
-import { useAuthStore } from '@store/useAuthStore';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom';
-import debounce from '@/utils/debounce';
 import { ResumeNewDTO } from '@/types/ResumeNew';
 import ResumeSubheading from '@/components/MyPage/ResumeSubheading';
-import ResumeInfoIcon from '@/components/MyPage/ResumeInfoIcon';
+import { useAuthStore } from '@store/useAuthStore';
+import { getPbImageURL } from '@/store/getPbImageURL';
 
-const createData: (
-  tableName: string,
-  data: Partial<ResumeNewDTO>
-) => Promise<ResumeNewDTO[] | null> = async (tableName, data) => {
-  const { data: insertedData } = await supabase
-    .from(tableName)
-    .insert(data)
-    .single();
-
-  return insertedData;
-};
+import phone from '@assets/mypage/resume-phone.svg';
+import mail from '@assets/mypage/resume-mail.svg';
+import github from '@assets/mypage/resume-github.svg';
+import blog from '@assets/mypage/resume-blog.svg';
+import plus from '@assets/mypage/resume-plus.svg';
 
 function ResemeDetailPage() {
-  const userId = useAuthStore((state) => state.user);
-  const userEmail = useAuthStore((state) => state.userEmail);
-  const navigate = useNavigate();
-  const [, setTitle] = useState('');
-  const [, setName] = useState('');
-  const [, setJob] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [, setPhone] = useState('');
-  const [, setMail] = useState('');
-  const [, setGithub] = useState('');
-  const [, setBlog] = useState('');
-  const [, setLink] = useState('');
-  const [linkCount, setLinkCount] = useState(0);
-  const [, setActivity] = useState('');
-  const [, setContent] = useState('');
-  const [, setIntroduce] = useState('');
-  const [stack, setStack] = useState<
-    'Javascript' | 'TypeScirpt' | 'React' | 'Next.js'
-  >('Javascript');
-  const [portfolioCount, setPortfolioCount] = useState(0);
+  const { itemId } = useParams();
+  const { user } = useAuthStore();
+  const [profileImageUrl, setProfileImageUrl] = useState('');
 
-  const titleRef = useRef<HTMLInputElement>(null);
-  const nameRef = useRef<HTMLInputElement>(null);
-  const jobRef = useRef<HTMLInputElement>(null);
-  const phoneRef = useRef<HTMLInputElement>(null);
-  const mailRef = useRef<HTMLInputElement>(null);
-  const githubRef = useRef<HTMLInputElement>(null);
-  const blogRef = useRef<HTMLInputElement>(null);
-  const linkRef = useRef<HTMLInputElement>(null);
-  const activityRef = useRef<HTMLTextAreaElement>(null);
-  const projectRef = useRef<HTMLTextAreaElement>(null);
-  const introduceRef = useRef<HTMLTextAreaElement>(null);
-  const SecureEmail = userEmail.replace(/@.*/, '');
+  useEffect(() => {
+    setProfileImageUrl(getPbImageURL('profile_resume', user));
+  }, []);
 
-  const debouncedSetTitle = debounce((value: string) => setTitle(value), 500);
-  const debouncedSetName = debounce((value: string) => setName(value), 500);
-  const debouncedSetJob = debounce((value: string) => setJob(value), 500);
-  const debouncedSetPhone = debounce((value: string) => setPhone(value), 500);
-  const debouncedSetMail = debounce((value: string) => setMail(value), 500);
-  const debouncedSetGithub = debounce((value: string) => setGithub(value), 500);
-  const debouncedSetBlog = debounce((value: string) => setBlog(value), 500);
-  const debouncedSetLink = debounce((value: string) => setLink(value), 500);
-  const debouncedSetActivity = debounce(
-    (value: string) => setActivity(value),
-    500
-  );
-  const debouncedSetContent = debounce(
-    (value: string) => setContent(value),
-    500
-  );
-  const debouncedSetIntroduce = debounce(
-    (value: string) => setIntroduce(value),
-    500
-  );
-
-  function phoneFormat(phoneNumber: string) {
-    const value = phoneNumber.replace(/[^0-9]/g, '');
-
-    const firstLength = value.length > 9 ? 3 : 4;
-    const formatted = [
-      value.slice(0, firstLength),
-      value.slice(firstLength, firstLength + 4),
-      value.slice(firstLength + 4),
-    ].join('-');
-
-    return value.length > 9 ? formatted : value;
-  }
-
-  const handlePhoneInput = (e: React.FormEvent<HTMLInputElement>) => {
-    const formatted = phoneFormat(e.currentTarget.value);
-    setPhoneNumber(formatted);
-    debouncedSetPhone(formatted);
-  };
-
-  const handlePlusLink = (e: React.FormEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setLinkCount((prevLinks) => prevLinks + 1);
-  };
-
-  const handlePlusStack = (e: React.FormEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setStack('React');
-  };
-
-  const handlePlusPortfolio = (e: React.FormEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setPortfolioCount((prevLinks) => prevLinks + 1);
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const title = titleRef.current?.value;
-    const name = nameRef.current?.value;
-    const job = jobRef.current?.value;
-    const phone = phoneRef.current?.value;
-    const mail = mailRef.current?.value;
-    const github = githubRef.current?.value;
-    const blog = blogRef.current?.value;
-    const link = linkRef.current?.value;
-    const activity = activityRef.current?.value;
-    const project = projectRef.current?.value;
-    const introduce = introduceRef.current?.value;
-
-    if (title && name && job && userId) {
-      const data = {
-        title,
-        name,
-        job,
-        phone,
-        mail,
-        github,
-        blog,
-        link,
-        activity,
-        project,
-        introduce,
-        user_id: userId,
-      };
-
-      try {
-        await createData('resume', data);
-        toast.success(`작성 완료 👌 ${stack}`);
-        setTimeout(() => {
-          navigate(`/mypage/${SecureEmail}/resume`);
-        }, 3000);
-      } catch (error) {
-        toast.error('작성 실패 😞');
+  const getResumeData: () => Promise<ResumeNewDTO[] | null> = async () => {
+    const { data, error } = await supabase
+      .from('resume')
+      .select('*')
+      .eq('user_id', user)
+      .eq('id', Number(itemId))
+      .returns<ResumeNewDTO[] | null>();
+    if (error) {
+      console.error('Error fetching data:', error);
+    } else {
+      if (data) {
+        console.log('fetching data');
       }
     }
+
+    return data;
   };
 
-  const handleTemporary = (e: React.FormEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-  };
+  const { data: resumeData, error } = useQuery('resume', getResumeData);
 
-  const handleReset = () => {
-    titleRef.current!.value = '';
-    nameRef.current!.value = '';
-    jobRef.current!.value = '';
-    projectRef.current!.value = '';
-  };
+  if (error) {
+    console.error('Error fetching data:', error);
+    return <div>죄송합니다. 잠시 후에 시도해주세요.</div>;
+  }
 
-  return (
-    <>
-      <Helmet>
-        <title>createNew ResumePage</title>
-      </Helmet>
-      <h1 className="sr-only">새 이력서 작성</h1>
-      <StyledFormArea onSubmit={handleSubmit}>
-        <h2 className="sr-only">제목</h2>
-        <StyledTitleInput
-          type="text"
-          placeholder="제목을 입력해주세요."
-          required
-          ref={titleRef}
-          onChange={(e) => debouncedSetTitle(e.target.value)}
-        />
-        <ResumeSubheading essential>기본 정보</ResumeSubheading>
-        <StyledSection>
-          <StyledInfoInput
-            type="text"
-            placeholder="이름을 입력해주세요."
-            required
-            ref={nameRef}
-            onChange={(e) => debouncedSetName(e.target.value)}
-          />
-          <StyledInfoInput
-            type="text"
-            placeholder="직무를 입력해주세요."
-            required
-            ref={jobRef}
-            onChange={(e) => debouncedSetJob(e.target.value)}
-          />
-          <span>개인정보</span>
-          <StyledPersonalContainer>
-            <StyledLabel>
-              <ResumeInfoIcon imgIcon="전화번호" />
-              <StyledPersonalInput
-                type="tel"
-                placeholder="숫자만 입력해주세요."
-                maxLength={14}
-                value={phoneNumber}
-                onInput={handlePhoneInput}
-                ref={phoneRef}
-                onChange={(e) => debouncedSetPhone(e.target.value)}
-              />
-            </StyledLabel>
-            <StyledLabel>
-              <ResumeInfoIcon imgIcon="메일" />
-              <StyledPersonalInput
-                type="email"
-                placeholder="이메일 주소를 입력해주세요."
-                ref={mailRef}
-                onChange={(e) => debouncedSetMail(e.target.value)}
-              />
-            </StyledLabel>
-            <StyledLabel>
-              <ResumeInfoIcon imgIcon="깃허브" />
-              <StyledPersonalInput
-                type="url"
-                placeholder="깃허브 주소를 입력해주세요."
-                ref={githubRef}
-                onChange={(e) => debouncedSetGithub(e.target.value)}
-              />
-            </StyledLabel>
-            <StyledLabel>
-              <ResumeInfoIcon imgIcon="블로그" />
-              <StyledPersonalInput
-                type="url"
-                placeholder="블로그 주소를 입력해주세요."
-                ref={blogRef}
-                onChange={(e) => debouncedSetBlog(e.target.value)}
-              />
-            </StyledLabel>
-            {Array(linkCount)
-              .fill(null)
-              .map((_, index) => (
-                <StyledLabel>
-                  <ResumeInfoIcon />
-                  <StyledPersonalInput
-                    key={index}
-                    type="url"
-                    placeholder="추가 링크 주소를 입력해주세요."
-                    ref={linkRef}
-                    onChange={(e) => debouncedSetLink(e.target.value)}
-                  />
-                </StyledLabel>
-              ))}
-            <StyledPlusButton onClick={handlePlusLink}>
-              <ResumeInfoIcon />
-              링크 추가
-            </StyledPlusButton>
-          </StyledPersonalContainer>
-        </StyledSection>
-        <ResumeSubheading>기술 스택</ResumeSubheading>
-        <StyledSection>
-          <StyledPlusButton onClick={handlePlusStack}>
-            <ResumeInfoIcon />
-            기술 스택 추가
-          </StyledPlusButton>
-        </StyledSection>
-        <label>
+  if (resumeData) {
+    return (
+      <>
+        <Helmet>
+          <title>createNew ResumePage</title>
+        </Helmet>
+        <h1 className="sr-only">새 이력서 작성</h1>
+        <StyledArea>
+          <StyledTitle>{resumeData[0].title}</StyledTitle>
+          <StyledFlexLayout>
+            <StyledProfileWrapper>
+              <Image src={profileImageUrl} alt="profile" />
+            </StyledProfileWrapper>
+            <StyledSection>
+              <StyledInfo>{resumeData[0].info_name}</StyledInfo>
+              <p>{resumeData[0].info_job}</p>
+              <InfoIconNav>
+                {resumeData[0].info_phone && (
+                  <StyledImg src={phone} alt="전화번호" />
+                )}
+                {resumeData[0].info_mail && (
+                  <StyledImg src={mail} alt="이메일" />
+                )}
+                {resumeData[0].info_github && (
+                  <StyledImg src={github} alt="깃헙" />
+                )}
+                {resumeData[0].info_blog && (
+                  <StyledImg src={blog} alt="블로그" />
+                )}
+                {resumeData[0].info_link && <StyledImg src={plus} alt="링크" />}
+              </InfoIconNav>
+            </StyledSection>
+          </StyledFlexLayout>
+
+          <ResumeSubheading>기술 스택</ResumeSubheading>
+          <StyledSection>
+            <p>작성한 내용이 없습니다.</p>
+          </StyledSection>
           <ResumeSubheading>경험/활동/교육</ResumeSubheading>
-          <StyledTextarea
-            ref={activityRef}
-            onChange={(e) => debouncedSetActivity(e.target.value)}
-          />
-        </label>
-        <label>
+          <StyledSection>
+            <StyledText>활동명: {resumeData[0].activity[0]}</StyledText>
+            <p>기관/장소명: {resumeData[0].activity[1]}</p>
+            <p>기간: {resumeData[0].activity[2]}</p>
+            <p>{resumeData[0].activity[3]}</p>
+          </StyledSection>
+
           <ResumeSubheading>프로젝트</ResumeSubheading>
-          <StyledTextarea
-            ref={projectRef}
-            onChange={(e) => debouncedSetContent(e.target.value)}
-          />
-        </label>
-        <label>
+          <StyledSection>
+            <StyledText>프로젝트명: {resumeData[0].project[0]}</StyledText>
+            <p>소속/기타: {resumeData[0].project[1]}</p>
+            <p>기간: {resumeData[0].project[2]}</p>
+            <p>{resumeData[0].project[3]}</p>
+          </StyledSection>
+
           <ResumeSubheading>자기소개</ResumeSubheading>
-          <StyledTextarea
-            ref={introduceRef}
-            onChange={(e) => debouncedSetIntroduce(e.target.value)}
-          />
-        </label>
-        <ResumeSubheading>포트폴리오</ResumeSubheading>
-        <StyledSection>
-          {Array(portfolioCount)
-            .fill(null)
-            .map((_, index) => (
-              <StyledLabel>
-                <ResumeInfoIcon />
-                <StyledPersonalInput
-                  key={index}
-                  type="url"
-                  placeholder="추가 링크 주소를 입력해주세요."
-                  ref={linkRef}
-                  onChange={(e) => debouncedSetLink(e.target.value)}
-                />
-              </StyledLabel>
-            ))}
-          <StyledPlusButton onClick={handlePlusPortfolio}>
-            <ResumeInfoIcon />
-            포트폴리오 추가 (링크)
-          </StyledPlusButton>
-        </StyledSection>
-        <ButtonArea>
-          <Button type="button" onClick={handleTemporary}>
-            임시저장
-          </Button>
-          <Button type="submit">저장</Button>
-          <Button type="button" onClick={handleReset}>
-            취소
-          </Button>
-        </ButtonArea>
-      </StyledFormArea>
-    </>
-  );
+          <StyledSection>
+            <StyledText>{resumeData[0].introduce[0]}</StyledText>
+            <p>{resumeData[0].introduce[1]}</p>
+          </StyledSection>
+
+          <ResumeSubheading>포트폴리오</ResumeSubheading>
+          <StyledSection>
+            <p>첨부한 링크가 없습니다.</p>
+          </StyledSection>
+          <ButtonArea>
+            <Button type="button" onClick={() => {}}>
+              수정
+            </Button>
+            <Button type="button" onClick={() => {}}>
+              삭제
+            </Button>
+          </ButtonArea>
+        </StyledArea>
+      </>
+    );
+  }
 }
 
 export default ResemeDetailPage;
 
-const StyledFormArea = styled.form`
+const StyledArea = styled.section`
   display: flex;
   flex-direction: column;
   gap: 1.25rem;
@@ -337,90 +143,101 @@ const StyledFormArea = styled.form`
 const StyledSection = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1.25rem;
-  padding: 1.25rem;
-  border: 3px solid #444;
+  gap: 1rem;
+  margin: 1rem 0 1rem 1rem;
+
+  @media ${(props) => props.theme.device.tablet} {
+    margin: 0.75rem 0 0.75rem 0.75rem;
+  }
+  @media ${(props) => props.theme.device.mobile} {
+    margin: 0.5rem 0 0.5rem 0.5rem;
+  }
+`;
+
+const StyledFlexLayout = styled.section`
+  display: flex;
+  gap: 2rem;
+  margin: 1rem 0 1rem 1rem;
+
+  @media ${(props) => props.theme.device.tablet} {
+    margin: 0.75rem 0 0.75rem 0.75rem;
+  }
+  @media ${(props) => props.theme.device.mobile} {
+    margin: 0.5rem 0 0.5rem 0.5rem;
+  }
+`;
+
+const StyledProfileWrapper = styled.div`
+  position: relative;
+  width: 15rem;
+  height: 15rem;
   border-radius: 0.625rem;
-
-  @media ${(props) => props.theme.device.tablet} {
-    padding: 1.25rem;
-  }
-  @media ${(props) => props.theme.device.mobile} {
-    padding: 0.625rem;
-  }
-
-  span {
-    font-size: 2rem;
-    font-weight: 600;
-
-    @media ${(props) => props.theme.device.tablet} {
-      font-size: 1.75rem;
-    }
-    @media ${(props) => props.theme.device.mobile} {
-      font-size: 1.125rem;
-    }
-  }
-`;
-
-const StyledPersonalContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.625rem;
-  width: auto;
-  margin-left: 0.625rem;
-`;
-
-const StyledLabel = styled.label`
-  display: flex;
-  gap: 0.625rem;
-  max-width: 50rem;
-  cursor: pointer;
-
-  @media ${(props) => props.theme.device.tablet} {
-    width: auto;
-  }
-  @media ${(props) => props.theme.device.mobile} {
-    width: auto;
-  }
-`;
-
-const StyledInput = styled.input`
-  padding: 0.3125rem 0.9375rem;
-  border: 1px solid #999;
-  border-radius: 0.3125rem;
   background: white;
-  outline: none;
 
-  &:focus {
-    animation: round 1s forwards;
+  @media ${(props) => props.theme.device.tablet} {
+    width: 11.25rem;
+    height: 11.25rem;
   }
-
-  @keyframes round {
-    from {
-      border-radius: 0.3125rem;
-    }
-    to {
-      border-radius: 1.875rem;
-    }
+  @media ${(props) => props.theme.device.mobile} {
+    width: 7.5rem;
+    height: 7.5rem;
   }
 `;
 
-const StyledTitleInput = styled(StyledInput)`
-  font-size: 2.75rem;
+const Image = styled.img`
+  position: absolute;
+  display: block;
+  color: white;
+  font-size: 10.625rem;
+  font-weight: 700;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+  object-fit: fill;
+  border-radius: 0.625rem;
+  transform: translate(-50%, -50%);
+
+  @media ${(props) => props.theme.device.tablet} {
+    font-size: 8.125rem;
+  }
+  @media ${(props) => props.theme.device.mobile} {
+    font-size: 5rem;
+  }
+`;
+
+const InfoIconNav = styled.nav`
+  display: flex;
+  gap: 0.625rem;
+  cursor: pointer;
+`;
+
+const StyledImg = styled.img`
+  width: 2.25rem;
+
+  @media ${(props) => props.theme.device.tablet} {
+    width: 2rem;
+  }
+  @media ${(props) => props.theme.device.mobile} {
+    width: 1.375rem;
+  }
+`;
+
+const StyledTitle = styled.h2`
+  font-size: 3.5rem;
   font-weight: bold;
 
   @media ${(props) => props.theme.device.tablet} {
-    font-size: 2.5rem;
+    font-size: 3rem;
   }
   @media ${(props) => props.theme.device.mobile} {
-    font-size: 1.5rem;
+    font-size: 2.5rem;
   }
 `;
 
-const StyledInfoInput = styled(StyledInput)`
-  max-width: 50rem;
+const StyledInfo = styled.h3`
   font-size: 2rem;
-  font-weight: 500;
+  font-weight: 600;
 
   @media ${(props) => props.theme.device.tablet} {
     font-size: 1.75rem;
@@ -430,53 +247,15 @@ const StyledInfoInput = styled(StyledInput)`
   }
 `;
 
-const StyledPersonalInput = styled(StyledInput)`
-  width: 100%;
+const StyledText = styled.p`
   font-size: 1.5rem;
+  font-weight: 500;
 
   @media ${(props) => props.theme.device.tablet} {
     font-size: 1.25rem;
   }
   @media ${(props) => props.theme.device.mobile} {
-    font-size: 0.875rem;
-  }
-`;
-
-const StyledPlusButton = styled.button`
-  display: flex;
-  align-items: center;
-  gap: 0.3125rem;
-  width: fit-content;
-  padding: 0.3125rem 0.9375rem;
-  border: none;
-  border-radius: 0.625rem;
-  box-shadow: 3px 3px 2px 1px rgba(137, 137, 138, 0.2);
-  background: white;
-  font-weight: 700;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #111;
-    color: white;
-  }
-`;
-
-const StyledTextarea = styled.textarea`
-  width: 100%;
-  min-height: 18.75rem;
-  resize: none;
-  box-sizing: border-box;
-  margin-top: 0.3125rem;
-  padding: 0.3125rem;
-  border: 1px solid black;
-  padding: 0.625rem;
-  font-size: 1.5rem;
-
-  @media ${(props) => props.theme.device.tablet} {
-    font-size: 1.25rem;
-  }
-  @media ${(props) => props.theme.device.mobile} {
-    font-size: 0.875rem;
+    font-size: 1rem;
   }
 `;
 
