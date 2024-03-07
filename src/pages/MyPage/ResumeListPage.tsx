ff46416c9d0faabc23a4de53bd29a59d6a4e1215
uniS2 <s2oie1010@gmail.com>
2024-03-07 22:21:36 +0900

    Feat: 이력서 리스트 페이지 - 작성한 이력서 있을 경우 페이지 작성 완료

--- src/pages/MyPage/ResumeListPage.tsx
+++ src/pages/MyPage/ResumeListPage.tsx
@@ -1,23 +1,83 @@
+import { supabase } from '@/client';
+import ResumeCategory from '@/components/MyPage/ResumeCategory';
+import { useAuthStore } from '@/store/useAuthStore';
+import { ResumeNewDTO } from '@/types/ResumeNew';
 import { Helmet } from 'react-helmet-async';
+import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 function ResumeListPage() {
+  const { user } = useAuthStore();
+
+  const getListData: () => Promise<ResumeNewDTO[] | null> = async () => {
+    const { data, error } = await supabase
+      .from('resume')
+      .select('*')
+      .eq('user_id', user)
+      .order('created_at', { ascending: false })
+      .returns<ResumeNewDTO[] | null>();
+    if (error) {
+      console.error('Error fetching data:', error);
+    } else {
+      if (data) {
+        console.log('fetching data');
+      }
+    }
+
+    return data;
+  };
+
+  const { data: resumeListData, error } = useQuery('resume', getListData);
+
+  if (error) {
+    console.error('Error fetching data:', error);
+    return <div>죄송합니다. 잠시 후에 시도해주세요.</div>;
+  }
+
   return (
-    <>
+    <StyledListSection>
       <Helmet>MyResumePage - JUNGLE</Helmet>
       <h1 className="sr-only">JUNGLE - 이력서 목록 페이지</h1>
-      <StyledMyResumeSection>
-        <StyledLink to={'/mypage/resume/new'}>글 작성</StyledLink>
-        <StyledLink to={'/main'}>메인으로 이동</StyledLink>
-      </StyledMyResumeSection>
-    </>
+      <StyledPositionLink to={'/mypage/resume/new'}>글 작성</StyledPositionLink>
+      {resumeListData ? (
+        <StyledResumeSection>
+          {resumeListData.map((resume, index) => (
+            <StyledWritingWrapper key={index}>
+              <ResumeCategory
+                href={`/mypage/resume/detail/${resume.id}`}
+                title={resume.title || undefined}
+                context={resume.project.join('\n') || undefined}
+                date={resume.updated_at ? resume.updated_at : resume.created_at}
+              />
+            </StyledWritingWrapper>
+          ))}
+        </StyledResumeSection>
+      ) : (
+        <StyledNoResumeSection>
+          <StyledLink to={'/mypage/resume/new'}>글 작성</StyledLink>
+          <StyledLink to={'/main'}>메인으로 이동</StyledLink>
+        </StyledNoResumeSection>
+      )}
+    </StyledListSection>
   );
 }
 
 export default ResumeListPage;
 
-const StyledMyResumeSection = styled.section`
+const StyledListSection = styled.section`
+  position: relative;
+`;
+
+const StyledResumeSection = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.875rem;
+  margin: 3.125rem;
+  align-items: center;
+`;
+
+const StyledNoResumeSection = styled.section`
   display: flex;
   flex-direction: column;
   gap: 1.875rem;
@@ -38,6 +98,24 @@ const StyledMyResumeSection = styled.section`
   }
 `;
 
+const StyledWritingWrapper = styled.div`
+  min-width: 17.5rem;
+  width: 17.5rem;
+  margin: 1.5625rem 3.125rem 1.5625rem 0;
+  padding: 1.875rem;
+  border-radius: 15px;
+  background: white;
+  color: var(--bs-black-200);
+  border: 2px black solid;
+
+  @media ${(props) => props.theme.device.mobile} {
+    min-width: 12.5rem;
+    width: 12.5rem;
+    margin: 1rem 0.5rem;
+    padding: 0.75rem;
+  }
+`;
+
 const StyledLink = styled(Link)`
   text-decoration: none;
   padding: 0.75rem 1.25rem;
@@ -55,4 +133,10 @@ const StyledLink = styled(Link)`
   @media ${(props) => props.theme.device.mobile} {
     padding: 0.25rem 0.75rem;
   }
+`;
+
+const StyledPositionLink = styled(StyledLink)`
+  position: absolute;
+  top: -2rem;
+  right: 0;
 `;
